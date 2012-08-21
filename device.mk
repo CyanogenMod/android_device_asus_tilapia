bead144d1ca3f10e5ea7a93c58b35d6bc0630f80
Ramanan Rajeswaran <ramanan@google.com>
2012-08-21 16:24:08 -0700

    Add overlay and various radio / ril settings.
    Change-Id: I02fb398b64f55b20846ef7a8b0d8a52d60d9d457

--- device.mk
+++ device.mk
@@ -19,3 +19,9 @@ $(call inherit-product, device/asus/grouper/device-common.mk)
 
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/tilapia/device-vendor.mk)
+
+PRODUCT_COPY_FILES += \
+    device/asus/tilapia/init.tilapia.rc:root/init.tilapia.rc
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/asus/tilapia/overlay
