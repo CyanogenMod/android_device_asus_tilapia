e945e2259d4b37a442ff9952797fa5efa3c314e7
Jean-Baptiste Queru <jbq@google.com>
2012-12-27 09:14:55 -0800

    Packages for JDP82
    Change-Id: I82fa1cc34b209b69cb33cfa222c4fe606bbdda46

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -21,17 +21,20 @@
 # 527662 = JOP40C
 # 533553 = JOP40D
 # end jb-mr1-dev
+# start jb-mr1.1-dev
+# 551245 = JDP82
+# end jb-mr1.1-dev
 
 source ../../../common/clear-factory-images-variables.sh
-BUILD=533553
+BUILD=551245
 DEVICE=tilapia
 PRODUCT=nakasig
-VERSION=jop40d
+VERSION=jdp82
 SRCPREFIX=signed-
 BOOTLOADERFILE=bootloader.bin
-BOOTLOADER=4.13
+BOOTLOADER=4.18
 RADIOFILE=radio.img
-RADIO=1231_0.10.0_1021
+RADIO=1231_0.17.0_1205
 SLEEPDURATION=10
 UNLOCKBOOTLOADER=true
 ERASEALL=true
