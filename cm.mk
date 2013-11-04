8691fa6cbe30557a49969c746e8678e0e9c7092f
Danesh M <daneshm90@gmail.com>
2013-11-04 13:30:15 -0800

    Tilapia : Switch to telephony.mk
    Since the apn-confs got merged, gsm.mk is a no-op.
    This patchset switches tilapia to telephony.mk
    
    Change-Id: I801dcf4115651a492f9e57770202addd4c841f20

--- cm.mk
+++ cm.mk
@@ -8,8 +8,8 @@ TARGET_SCREEN_WIDTH := 800
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit GSM common stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit telephony common stuff
+$(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
